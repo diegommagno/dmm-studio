a60414091093f88a46feaad07c2e921b978190b7
Diego M. Magno <diegomartinelli@live.com>
2023-11-27 14:20:58 +0000

    :sparkles: feat: Add Section Testimonial

--- websites/portfolio/01/assets/js/index.js
+++ websites/portfolio/01/assets/js/index.js
@@ -22,6 +22,16 @@ modalClose.forEach((modalClose) => {
 })
 
 /*=============== SWIPER TESTIMONIAL ===============*/
+const swiperTestimonial = new Swiper('.testimonial__swiper', {
+    loop: true,
+    spaceBetween: 32,
+    grabCursor: true,
 
+    pagination: {
+        el: '.swiper-pagination',
+        dynamicBullets: true,
+        clickable: true,
+    },
+})
 
 /*=============== SHOW SCROLL UP ===============*/ 

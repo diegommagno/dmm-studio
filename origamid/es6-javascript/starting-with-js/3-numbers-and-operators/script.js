e2043e2c26db7035e575eac29cddd23ec43373ab
diegommagno <contact@diegommagno.com>
2024-01-22 23:59:44 +0000

    :sparkles: Numbers and Operators

---
+++ origamid/es6-javascript/starting-with-js/3-numbers-and-operators/script.js
@@ -0,0 +1,16 @@
+// Operators
+
+let sum = 100 + 50; // 150
+let subtract = 100 - 50; // 50
+let multiplication = 100 * 2; // 200
+let division = 100 / 2; // 50
+let exponent = 2 ** 4; // 16
+let remainder = 14 % 5; // 4 - let modulo in PT/BR - Restante da divisão.
+
+// Operators (Strings)
+
+let sum2 = '100' +  50; // 10050 - This is a string.
+let subtraction2 = '100' - 50; // 50 - This is a number.
+let multiplication2 = '100' * '2'; // 200 - This is a number.
+let division2 = 'Comprei 10' / 2; // NaN (Not a Number)
+let division3 = '100' / 2; // 50 - This is a number.

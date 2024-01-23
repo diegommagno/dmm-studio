4def5e8b025aafab6dcd10773e3b01fef66aef4d
diegommagno <contact@diegommagno.com>
2024-01-23 00:34:25 +0000

    :sparkles: Numbers and Operators 2 / Boolean and Conditionals

--- origamid/es6-javascript/starting-with-js/3-numbers-and-operators/script.js
+++ origamid/es6-javascript/starting-with-js/3-numbers-and-operators/script.js
@@ -10,7 +10,34 @@ let remainder = 14 % 5; // 4 - let modulo in PT/BR - Restante da divisão.
 // Operators (Strings)
 
 let sum2 = '100' +  50; // 10050 - This is a string.
-let subtraction2 = '100' - 50; // 50 - This is a number.
+let subtraction2 = '100' - 50; // 50 - This is a number. Colocar o +'100' antes da string transforma ela em número.
 let multiplication2 = '100' * '2'; // 200 - This is a number.
 let division2 = 'Comprei 10' / 2; // NaN (Not a Number)
 let division3 = '100' / 2; // 50 - This is a number.
+
+// Order of operations
+// The order of operations is the same as in mathematics.
+
+let total = 20 + 5 * 2; // 30
+let total2 = (20 + 5) * 2; // 50
+let total3 = 20 / 2 * 5; // 50
+let total4 = 10 + 10 * 2 + 20 / 2; // 40
+
+// Unary operations
+// Unary operations are operations that occur with only one value.
+
+let increment = 5;
+console.log(increment++); // 5 - The increment is done after the operation.
+console.log(increment); // 6
+
+let increment2 = 5;
+console.log(++increment2); // 6 - The increment is done before the operation.
+
+let decrement = 5;
+console.log(decrement--); // 5 - The decrement is done after the operation.
+
+let decrement2 = 5;
+console.log(--decrement2); // 4 - The decrement is done before the operation.
+
+// Lembrando que se a operação x++ for feita antes do console.log, basta dar console.log(variável) que será mostrado o valor correto, visto a operação já ter sido realizada.
+
---
+++ origamid/es6-javascript/starting-with-js/4-boolean-and-conditionals/script.js
@@ -0,0 +1,59 @@
+// Boolean can be either true or false
+
+let isBoolean = true;
+let isString = false;
+
+// Conditionals if, else, else if
+
+let isTrue = true;
+
+if (isTrue) {
+    console.log('Is true.');
+} else {
+    console.log('Is false.');
+}
+
+// Truthy and Falsy
+// Falsy: false, 0, '', "", ``, null, undefined, NaN
+// Truthy: Everything else
+
+let name = ''; // Empty string is falsy, so it returns 'Name is undefined.'
+let name2 = '  '; // String with spaces is truthy, so it returns the spaces.
+let object = {}; // Empty object is truthy, so it returns the object.
+
+if(name) {
+    console.log(name)
+} else {
+    console.log('Name is undefined.')
+}
+
+// Negation operator !
+// !true // false
+// !false // true
+
+// Double negation !! - Usually used to check if the value is a truthy or falsy
+// !!true // true
+// !!false // false
+
+// Comparison operators
+// > Greater than
+// >= Greater than or equal to
+// < Less than
+// <= Less than or equal to
+// == Equal to
+// === Equal to and of the same type
+let x = '10'; 
+
+console.log(x == 10); // True
+console.log(x === 10); // False, as the type is different
+console.log(x != 10); // False
+console.log(x !== 10); // True, as the type is different
+
+// != Different from (same logic as ==)
+// !== Different from and of the same type (same logic as ===)
+
+// Logical operators
+// && And
+// || Or
+// ! Not
+

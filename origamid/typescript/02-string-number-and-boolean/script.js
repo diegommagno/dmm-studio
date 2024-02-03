b3c0ce4ad97d11ffc047eb4f9ddb3361ea446e57
diegommagno <contact@diegommagno.com>
2024-02-03 22:51:08 +0000

    :sparkles: Add string-number-and-boolean

--- origamid/typescript/02-string-number-and-boolean/script.js
+++ origamid/typescript/02-string-number-and-boolean/script.js
@@ -4,3 +4,10 @@ const phrase = 'Front-end';
 const price02 = 500;
 const condition = true;
 // typeof operator
+console.log(typeof phrase); // string
+console.log(typeof price02); // number
+console.log(typeof condition); // boolean
+// Can be used as a type guard, to only to something if the type is correct
+if (typeof phrase === 'string') {
+    phrase.toLowerCase();
+}
--- origamid/typescript/02-string-number-and-boolean/script.ts
+++ origamid/typescript/02-string-number-and-boolean/script.ts
@@ -4,3 +4,11 @@ const price02: number = 500;
 const condition: boolean = true;
 
 // typeof operator
+console.log(typeof phrase); // string
+console.log(typeof price02); // number
+console.log(typeof condition); // boolean
+
+// Can be used as a type guard, to only to something if the type is correct
+if (typeof phrase === 'string') {
+    phrase.toLowerCase();
+}

7c67281254422d5444a6d01bc65a0f50b10a7ab3
diegommagno <contact@diegommagno.com>
2024-02-04 15:27:07 +0000

    :sparkles: Add Union Types

--- origamid/typescript/01-basic-typescript/03-union-types/script.ts
+++ origamid/typescript/01-basic-typescript/03-union-types/script.ts
@@ -6,7 +6,22 @@ total02 = '200';
 function isNumber(value: string | number) {
     if(typeof value === 'number') {
         return true;
+    } else {
+        return false; // Add return false, otherwise it will return undefined
     }
 }
 
 console.log(isNumber('200')); // undefined
+
+// DOM
+// Um local que se usa muito o Union types é no DOM, porque o JS não consegue analisar o HTML quando estamos fazendo a seleção.
+
+const button = document.createElement('button');
+
+if (button) {
+    button.click(); // If button exists, do button.click()
+}
+
+// Jeito mais limpo
+
+button?.click(); // If button exists (not null or undefined), do button.click()

a1058342d6a04a90aca957cc9521cb389e0b7c32
diegommagno <contact@diegommagno.com>
2024-02-05 21:46:42 +0000

    :sparkle: Union Types Task

--- origamid/typescript/01-basic-typescript/03-union-types/script.ts
+++ origamid/typescript/01-basic-typescript/03-union-types/script.ts
@@ -25,3 +25,26 @@ if (button) {
 // Jeito mais limpo
 
 button?.click(); // If button exists (not null or undefined), do button.click()
+
+// Tasks
+// 1. Create a function named toNumber
+function toNumber(value: string | number) {
+    if(typeof value === 'number') {
+        return value
+    } else if (typeof value === 'string') {
+        return Number(value)
+    } else {
+        throw "Value needs to be either a number or a string"
+    }
+}
+
+console.log(toNumber("200")); // 200 (number)
+
+// 2. This function should receive a parameter named value that can be a string or a number
+
+// 3. If the value is a number, the function should return it
+
+// 4. If the value is a string, it should convert it to number and return it
+
+// 5. If the value is neither a number nor a string, the function should return an error. (throw "Value needs to be either a number or a string").
+
